064af9050f89f9831cc19b0b7c72d28c9b73dcc4
Shahrukh Yousafzai <shahrukhyousafzaiofficial@gmail.com>
2021-01-13 08:06:50 +0500

    Sample Uploaded

---
+++ Sample/PocketDBSample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ShahrukhYousafzai.PocketDB;
+
+namespace PocketDBSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //opening / creating
+            PocketDatabase DB = new PocketDatabase("SampleData"); 
+
+            //saving & loading
+            DB.Set("sample", "hello world");
+            string savedValue = DB.Get<string>("sample");
+
+            Console.WriteLine(savedValue);
+            Console.ReadLine();
+
+            //closing
+            DB.Close();
+
+        }
+    }
+}
